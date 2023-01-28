bdb3b6af96d168c3264dded9875cf4868a65ce4d
Dighitalsoluciones <hernanglomba@gmail.com>
2023-01-28 12:08:39 -0300

    subida inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlotMachineComponent } from './components/slot-machine/slot-machine.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlotMachineComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/slot-machine/slot-machine.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slot-machine',
+  templateUrl: './slot-machine.component.html',
+  styleUrls: ['./slot-machine.component.css']
+})
+export class SlotMachineComponent implements OnInit {
+ 
+  symbols = [
+    { name: "siete", image: "assets/siete.png" },
+    { name: "bar", image: "assets/bar.png" },
+    { name: "sandia", image: "assets/sandia.png" },
+    { name: "cherry", image: "assets/cherry.png" },
+    { name: "herradura", image: "assets/herradura.png" }
+  ];
+  weights = [0.2, 1, 0.25, 0.1, 0.15];
+
+  winningCombinations = {
+    "siete siete siete": 20,
+    "bar bar bar": 30,
+    "sandia sandia sandia": 25,
+    "cherry cherry cherry": 10,
+    "herradura herradura herradura": 15
+  };
+
+  reels: { name: string, image: string }[][] = [[], [], []];
+  isWinner = false;
+  winnings = 0;
+  rodillo1 = [];
+  rodillo2= [];
+  rodillo3= [];
+
+  isSpinning = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.reels;
+    console.log(this.randomSymbol());
+    this.rodillo1 = this.randomSymbol();
+    this.rodillo2 = this.randomSymbol();
+    this.rodillo3 = this.randomSymbol();
+
+  }
+
+  girar(){
+  location.reload()
+  }
+
+  randomSymbol() {
+    const totalWeight = this.weights.reduce((acc, cur) => acc + cur, 0);
+    const rand = Math.random() * totalWeight;
+    let symbol;
+    let weightSum = 0;
+    for (let i = 0; i < this.symbols.length; i++) {
+      weightSum += this.weights[i];
+      if (rand <= weightSum) {
+        symbol = this.symbols[i];
+        break;
+      }
+    }
+    return symbol;
+  }
+
+
+  spin() {
+    this.isSpinning = true;
+    const spinButton = document.getElementById("spin-button");
+    spinButton.setAttribute("disabled", "true");
+    const symbols = document.querySelectorAll(".spin");
+    symbols.forEach(symbol => symbol.classList.add("spin-animation"));
+
+    // Genera los simbolos aleatorios para cada reel
+    this.reels = [[this.randomSymbol(), this.randomSymbol(), this.randomSymbol()],
+                  [this.randomSymbol(), this.randomSymbol(), this.randomSymbol()],
+                  [this.randomSymbol(), this.randomSymbol(), this.randomSymbol()]];
+
+    setTimeout(() => {
+      symbols.forEach(symbol => symbol.classList.remove("spin-animation"));
+      this.isSpinning = false;
+      spinButton.removeAttribute("disabled");
+
+      // Chequea si hay una combinacion ganadora
+      this.checkWinningCombination(this.reels);
+    }, 3000);
+    console.log(this.randomSymbol());
+  }
+
+  checkWinningCombination(reels) {
+    // Toma los nombres de los símbolos en cada uno de los carretes
+    const reel1 = this.reels[0]['name'];
+    const reel2 = this.reels[1]['name'];
+    const reel3 = this.reels[2]['name'];
+    // Concatena los nombres de los símbolos para formar la combinación
+    const combination = reel1 + ' ' + reel2 + ' ' + reel3;
+
+    // Comprueba si la combinación es una ganadora
+    if (this.winningCombinations[combination]) {
+      this.isWinner = true;
+      this.winnings = this.winningCombinations[combination];
+    } else {
+      this.isWinner = false;
+      this.winnings = 0;
+    }
+  }
+  
+}
